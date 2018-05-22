a2184cb5e59dcda9f31791ac5806cb7904a74ef3
wuwenbin <765934806@qq.com>
2018-05-22 17:22:18 +0800

    添加SQL类，组装复杂sql语句用

---
+++ template-orm/src/main/java/me/wuwenbin/modules/sql/SQL.java
@@ -0,0 +1,13 @@
+package me.wuwenbin.modules.sql;
+
+/**
+ * created by Wuwenbin on 2018/5/22 at 17:20
+ */
+public class SQL {
+
+    private String sql;
+
+    public SQL init() {
+        return new SQL();
+    }
+}
